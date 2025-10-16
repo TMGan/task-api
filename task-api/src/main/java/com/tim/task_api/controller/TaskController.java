e4d51b466fabd05d1fedc4d3a00067e24c724d02
TMGan <gangadeentim@gmail.com>
2025-10-16 19:33:19 -0400

    implemented POST endpoint for creating tasks

--- task-api/src/main/java/com/tim/task_api/controller/TaskController.java
+++ task-api/src/main/java/com/tim/task_api/controller/TaskController.java
@@ -1,4 +1,24 @@
 package com.tim.task_api.controller;
 
+import com.tim.task_api.model.Task;
+import com.tim.task_api.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+
 }
--- task-api/src/main/java/com/tim/task_api/service/TaskService.java
+++ task-api/src/main/java/com/tim/task_api/service/TaskService.java
@@ -1,4 +1,29 @@
 package com.tim.task_api.service;
 
+import com.tim.task_api.model.Task;
+import com.tim.task_api.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
+    public Task createTask(Task task) {
+    if(task.getTitle()==null || task.getTitle().trim().isEmpty()) {
+        throw new IllegalArgumentException("Task title cannot be empty");
+    }
+    if (task.getStatus()== null) {
+        task.setStatus(Task.TaskStatus.PENDING);
+    }
+    if (task.getPriority() == null) {
+        task.setPriority(Task.Priority.MEDIUM);
+    }
+    return taskRepository.save(task);
+    }
 }
