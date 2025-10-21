36da52e1fdfd5dc2b1a1a06d62f30316871484b5
TMGan <gangadeentim@gmail.com>
2025-10-20 22:09:04 -0400

    Completed full CRUD API for Task Management

--- task-api/src/main/java/com/tim/task_api/controller/TaskController.java
+++ task-api/src/main/java/com/tim/task_api/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.tim.task_api.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
@@ -20,5 +22,25 @@ public class TaskController {
         return taskService.createTask(task);
     }
 
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        return taskService.updateTask(id, updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask (@PathVariable Long id) {
+         taskService.deleteTask(id);
+    }
 
 }
--- task-api/src/main/java/com/tim/task_api/service/TaskService.java
+++ task-api/src/main/java/com/tim/task_api/service/TaskService.java
@@ -4,6 +4,8 @@ import com.tim.task_api.model.Task;
 import com.tim.task_api.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
 
@@ -25,5 +27,28 @@ public class TaskService {
         task.setPriority(Task.Priority.MEDIUM);
     }
     return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+        Task existingTask = getTaskById(id);
+        existingTask.setTitle(updatedTask.getTitle());
+        existingTask.setDescription(updatedTask.getDescription());
+        existingTask.setStatus(updatedTask.getStatus());
+        existingTask.setPriority(updatedTask.getPriority());
+        return taskRepository.save(existingTask);
+    }
+
+    public void deleteTask(Long id) {
+        Task existingTask = getTaskById(id);
+        taskRepository.deleteById(id);
+
     }
 }
